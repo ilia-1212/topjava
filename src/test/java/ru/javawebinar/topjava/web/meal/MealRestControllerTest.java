10ab555c2d0a1207b1bcdfc92f387b67e8f56d23
ILia-1212 <i.sentyakov@cft.ru>
2023-04-17 18:20:53 +0500

    3_11_add_postgresql.patch

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealRestControllerTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
