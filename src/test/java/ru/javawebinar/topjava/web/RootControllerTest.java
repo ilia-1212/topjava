8a7e6a1fcdfdebe5c2de9ec9b9b17d3ee3158b0f
Сентяков Илья <i.sentyakov@cft.ru>
2023-11-17 01:28:14 +0300

    HW09/06 meal tests
    MealRestControllerTest.java
    RootControllerTest.java

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.jupiter.api.Test;
-import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
