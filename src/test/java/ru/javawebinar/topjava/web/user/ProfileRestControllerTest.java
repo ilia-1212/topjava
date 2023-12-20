d0386570ce48c6855373f5d5575d6b80244e094a
Сентяков Илья <i.sentyakov@cft.ru>
2023-12-20 14:31:39 +0300

    HW10 add user Profile rest tests

--- src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
@@ -65,6 +65,17 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    void registerWithError() throws Exception {
+        UserTo newTo = new UserTo(null, "", "", "newPassword", 1500);
+        User newUser = UsersUtil.createNewFromTo(newTo);
+        perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
     void update() throws Exception {
         UserTo updatedTo = new UserTo(null, "newName", "user@yandex.ru", "newPassword", 1500);
         perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
@@ -74,6 +85,16 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                 .andExpect(status().isNoContent());
 
         USER_MATCHER.assertMatch(userService.get(USER_ID), UsersUtil.updateFromTo(new User(user), updatedTo));
+    }
+
+    @Test
+    void updateWithError() throws Exception {
+        UserTo updatedTo = new UserTo(null, "", "", "newPassword", 1500);
+        perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(user))
+                .content(JsonUtil.writeValue(updatedTo)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
