877fed0a8c8e6ceffa716b35f818711da72c72dd
Сентяков Илья <i.sentyakov@cft.ru>
2024-01-09 12:05:07 +0300

    Revert "apply 11_10_auth_user.patch"
    This reverts commit 86151aae

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -8,28 +7,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
-import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.to.UserTo;
-import springfox.documentation.annotations.ApiIgnore;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
-@ApiIgnore
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("/profile")
 public class ProfileUIController extends AbstractUserController {
 
     @GetMapping
-    public String profile(ModelMap model, @AuthenticationPrincipal AuthorizedUser authUser) {
-        model.addAttribute("userTo", authUser.getUserTo());
+    public String profile() {
         return "profile";
     }
 
     @PostMapping
-    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status, @AuthenticationPrincipal AuthorizedUser authUser) {
+    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
         if (result.hasErrors()) {
             return "profile";
         }
-        super.update(userTo, authUser.getId());
-        authUser.setTo(userTo);
+        super.update(userTo, SecurityUtil.authUserId());
+        SecurityUtil.get().setTo(userTo);
         status.setComplete();
         return "redirect:/meals";
     }
