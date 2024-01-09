0c6492b39ba270205234906692d390c915c6b448
Сентяков Илья <i.sentyakov@cft.ru>
2024-01-09 12:05:13 +0300

    Revert "apply 11_08_i18n.patch"
    This reverts commit a318616f0f8c66a858ee8b516e20146cffbbe780.

--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
@@ -1,18 +1,8 @@
 package ru.javawebinar.topjava.util.exception;
 
 public enum ErrorType {
-    APP_ERROR("error.appError"),
-    DATA_NOT_FOUND("error.dataNotFound"),
-    DATA_ERROR("error.dataError"),
-    VALIDATION_ERROR("error.validationError");
-
-    private final String errorCode;
-
-    ErrorType(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
+    APP_ERROR,
+    DATA_NOT_FOUND,
+    DATA_ERROR,
+    VALIDATION_ERROR
 }
